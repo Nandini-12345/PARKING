6ab0fecd96369777c9bd7e034302193d92efae21
Nandhini S <nandhini.s@axiscades.in>
2024-02-28 11:59:26 +0530

    Initial

---
+++ Hubs/StatusHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ParkingDemo.Hubs;
+
+public class StatusHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
---
+++ wwwroot/js/state.js
@@ -0,0 +1,29 @@
+﻿let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub/status")
+    .build();
+
+connection.start()
+    .then(() => {
+        console.log("SignalR connected");
+
+        connection.on("ReceiveMessage", (address, status) => {
+            console.log("Received data from SignalR:", address);
+
+            try {
+                let spanElement = document.getElementById(`d-${address}`);
+                spanElement.innerText = status;
+
+                if (status === "ONLINE") {
+                    spanElement.style.background = "green";
+                }
+                else if (status === "OFFLINE"){
+                    spanElement.style.background = "red";
+                }
+            } catch (error) {
+                console.error("Error finding the element:", error);
+            }
+        });
+    })
+    .catch((err) => {
+        console.error("Error starting SignalR connection:", err.toString());
+    });
